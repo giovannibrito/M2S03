829eca603debdc39514b2ac7488cdfbc6882ae76
giovannibrito <giovannileonibrito@bol.com.br>
2023-02-15 09:59:39 -0300

    Adiciona ex 1

---
+++ src/Clinica.java
@@ -0,0 +1,11 @@
+public class Clinica {
+    String nomeDono;
+    String nomeClinica;
+    String cnpj;
+    String logradouro;
+    Integer codigoClinica;
+
+    public static void main(String[] args) {
+
+    }
+}
